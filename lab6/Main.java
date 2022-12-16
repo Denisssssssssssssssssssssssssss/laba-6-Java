703c2ed4c11a93ff56212bdf78634d8899c52ccf
Denisssssssssssssssssssssssssss <112960256+Denisssssssssssssssssssssssssss@users.noreply.github.com>
2022-12-16 12:32:25 +0700

    Add files via upload

--- lab6/Main.java
+++ lab6/Main.java
@@ -5,29 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /** Мелкое копирование **/
-        Jobtitle jt = new Jobtitle(10);
-        try {
-            Jobtitle jt1 = (Jobtitle) jt.clone();
-            jt.output();
-            jt.input();
-            jt.output();
-            jt1.output();
-        }
-        catch(CloneNotSupportedException e){
-            System.out.println(e);
-        }
-        /** Глубокое копирование **/
-        Jobtitle jt2 = new Jobtitle(10);
-        try {
-            Jobtitle jt3 = (Jobtitle) jt2.deepclone();
-            jt2.output();
-            jt2.input();
-            jt2.output();
-            jt3.output();
-        }
-        catch(CloneNotSupportedException e1){
-            System.out.println(e1);
-        }
+        Meleeweapon AA = new Meleeweapon(50);
+        Firearms BB = new Firearms(50);
+        System.out.println("\n" + AA.toString() + "\n");
+        System.out.println("\n" + BB.toString() + "\n");
     }
 }
